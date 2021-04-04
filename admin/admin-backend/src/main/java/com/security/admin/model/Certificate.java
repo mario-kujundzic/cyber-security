2246ae9530a92ec970d3c0cd160dd656581a571c
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2021-04-04 17:22:09 +0200

    Fix

--- admin/admin-backend/src/main/java/com/security/admin/model/Certificate.java
+++ admin/admin-backend/src/main/java/com/security/admin/model/Certificate.java
@@ -2,11 +2,13 @@ package com.security.admin.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.NonNull;
 
 @Entity
 @Table(name = "certificates")
@@ -14,35 +16,31 @@ import lombok.NonNull;
 @NoArgsConstructor
 public class Certificate {
 	
-	@NonNull
-	@Column(columnDefinition = "BLOB(2048) NOT NULL")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column
 	private byte[] encodedPublicKey;
 	
-	@NonNull
 	@Column
 	private String serialNumber;
 
-	@NonNull
 	@Column
 	private String commonName;
 
-	@NonNull
 	@Column
 	private String locality;
 
-	@NonNull
 	@Column
 	private String state;
 
-	@NonNull
 	@Column
 	private String organizationName;
 
-	@NonNull
 	@Column
 	private String organizationUnitName;
 
-	@NonNull
 	@Column
 	private String email;
 
@@ -51,5 +49,6 @@ public class Certificate {
 
 	@Column(columnDefinition = "BOOLEAN DEFAULT false")
 	private boolean revocationStatus;
+	
 
 }
