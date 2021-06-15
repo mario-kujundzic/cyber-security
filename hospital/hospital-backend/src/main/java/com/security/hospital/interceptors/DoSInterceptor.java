f8ba91f2b1f20dff4f5956a3f3412eb7c0470c92
Natasa Ivanovic <natasaivanovic98@yahoo.com>
2021-06-15 21:35:23 +0200

    DOS handling

--- hospital/hospital-backend/src/main/java/com/security/hospital/interceptors/DoSInterceptor.java
+++ hospital/hospital-backend/src/main/java/com/security/hospital/interceptors/DoSInterceptor.java
@@ -1,21 +1,47 @@
 package com.security.hospital.interceptors;
 
-import com.security.hospital.service.LogService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.security.hospital.MaliciousIPHandler;
+import com.security.hospital.events.DosEvent;
+import com.security.hospital.events.DosEventStatus;
+import com.security.hospital.service.KieSessionService;
+import com.security.hospital.service.LogService;
 
 @Component
 public class DoSInterceptor implements HandlerInterceptor {
     @Autowired
     private LogService logService;
+    
+    @Autowired
+    private KieSessionService session;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object object) {
         //logService.logHospitalInfo(request.getMethod() + " " + request.getRequestURI() + " from " + request.getRemoteAddr() + request.getRemotePort());
+    	String ip = request.getRemoteAddr();
+    	
+    	if(MaliciousIPHandler.ips.contains(ip)) {
+    		return false;
+    	}
+    	DosEvent de = new DosEvent(ip);
+    	DosEventStatus des = new DosEventStatus(ip);
+    	session.insert(de);
+    	session.insert(des);
+    	session.setAgendaFocus("dos-agenda");
+    	session.fireAllRules();
+    	if (des.isAttack()) {
+    		session.removeDosEvents(ip);
+    		logService.logGeneralError("DoS suspected from IP: " + ip);
+    		MaliciousIPHandler.ips.add(ip);
+    		MaliciousIPHandler.writeIP();
+    		return false;
+    	}
         return true;
     }
 }
--- hospital/hospital-backend/src/main/java/com/security/hospital/service/KieSessionService.java
+++ hospital/hospital-backend/src/main/java/com/security/hospital/service/KieSessionService.java
@@ -9,6 +9,7 @@ import org.kie.api.runtime.rule.FactHandle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.security.hospital.events.DosEvent;
 import com.security.hospital.events.InvalidLoginEvent;
 import com.security.hospital.events.MaliciousLoginEvent;
 
@@ -81,6 +82,23 @@ public class KieSessionService {
 			public boolean accept(Object object) {
 				if (object.getClass().equals(MaliciousLoginEvent.class)) {
 					MaliciousLoginEvent event = (MaliciousLoginEvent) object;
+					return event.getIPAddress().equals(IPAddress);
+				}
+				return false;
+			}
+		};
+		Collection<FactHandle> events = this.kieSession.getFactHandles(filter);
+		for (FactHandle handle : events) {
+			this.kieSession.delete(handle);			
+		}
+	}
+	
+	public void removeDosEvents(String IPAddress) {
+		ObjectFilter filter = new ObjectFilter() {
+			@Override
+			public boolean accept(Object object) {
+				if (object.getClass().equals(DosEvent.class)) {
+					DosEvent event = (DosEvent) object;
 					return event.getIPAddress().equals(IPAddress);
 				}
 				return false;
